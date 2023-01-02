28c173d75a548b18c80490e6db022ce272ade1e5
Venen Jean <samvenom69@gmail.com>
2023-01-02 17:19:33 +0100

    Simple nothing
    Changed some basics functions

--- SimpleServer/app.js
+++ SimpleServer/app.js
@@ -3,3 +3,13 @@ const app = express();
 
 const host = "127.0.0.1";
 const port = 4208;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Hello Express!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});

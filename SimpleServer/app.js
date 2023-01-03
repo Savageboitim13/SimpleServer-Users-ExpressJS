e5d4ee4268c46a0a5aa60f0585bc6e29be93f074
Venen Jean <samvenom69@gmail.com>
2023-01-03 07:00:03 +0100

    made it better
    Added some basic systems.

--- SimpleServer/app.js
+++ SimpleServer/app.js
@@ -5,11 +5,35 @@ const host = "127.0.0.1";
 const port = 4208;
 
 app.set("view engine", "ejs");
+app.use(logger);
 
-app.get("/", (req, res) => {
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", logger, (req, res) => {
   res.render("index", { title: "Hello Express!" });
 });
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
+
+/**
+ * res.status(STATUSCODE)
+ * res.send(MESSAGE)
+ * res.download(FILEPATH)
+ * res.render(FILEPATH)
+ * res.json(JSON)
+ * app.set(SETTINGNAME, NAME)
+ *
+ * <%= locals.NAME %>
+ */
--- SimpleServer/routes/users.js
+++ SimpleServer/routes/users.js
@@ -1,9 +1,70 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("User List");
+});
+
+router.get("/new", (req, res) => {
+  res.render("users/new");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body.firstName);
+  res.send("Create new User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get user with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with ID ${req.params.id}`);
+  });
+
+const users = [
+  {
+    fname: "Kyle",
+    lname: "none",
+    age: 32,
+    gender: "male",
+    job: "YouTuber",
+    email: "webDev@mail.com",
+    username: "WebDev",
+    password: "Simplified",
+  },
+  {
+    fname: "CodingLife",
+    lname: "nonde",
+    age: 13,
+    gender: "male",
+    job: "Hobby Developer",
+    email: "codingHome@gmail.com",
+    username: "ServerOwner",
+    password: "CodingHomeUniverse",
+  },
+  {
+    fname: "Venen",
+    lname: "Jean",
+    age: 15,
+    gender: "male",
+    job: "Full Stack Developer",
+    email: "SamVenom69@gmail.com",
+    username: "rest-client",
+    password: "beta-tester",
+  },
+];
+router.param("id", (req, res, next, id) => {
+  // Random Variable = users[id]
+  req.user = users[id];
+  next();
 });
 
 module.exports = router;
